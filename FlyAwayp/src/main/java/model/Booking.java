ee1899e17db61968974bc906a7a80e091f4f1a15
mogga gowtham <moggagowtham@gmail.com>
2022-10-17 03:12:08 +0530

    first commit

---
+++ FlyAwayp/src/main/java/model/Booking.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Booking {
+	
+    // flight details
+    public static String flight_booking_id;
+    public static String ticket_price;//-->database
+    public static String flight_name;
+    
+  
+    
+    // Booking  details
+    public static String passenger_name; //---> pname -->GowthamMogga
+    public static String passenger_email; //---> email -->moggagowtham@gmail.com
+    public static String passenger_phone; //----> phone --9705914973
+
+    // payment details
+    public static String name_on_card;
+    public static String card_details;
+    
+    }
+
+
+
+
+
